1869f3ecbd0abf528f2e29242f6d57949b3caa26
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-06-10 17:01:58 +0300

    add jQuery code for the terms and their definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 
-  $(document).ready(function(){
-    $("h2").click(function(){
-      $("p").toggle();
+$(document).ready(function () {
+    $("h2").click(function () {
+        $("p1").toggle();
     });
-  });
+});
+
+$(document).ready(function () {
+    $("h3").click(function () {
+        $("p2").toggle();
+    });
+});
+$(document).ready(function () {
+    $("h4").click(function () {
+        $("p3").toggle();
+    });
+});
+$(document).ready(function () {
+    $("h5").click(function () {
+        $("p4").toggle();
+    });
+});
+$(document).ready(function () {
+    $("h5").click(function () {
+        $("p5").toggle();
+    });
+});
